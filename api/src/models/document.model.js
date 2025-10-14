cdcb6f0c8fd1ab2417b6800337701709e361e5e6
amiramiour <amiouramirtahar@gmail.com>
2025-10-14 13:53:37 +0200

    documentModel

---
+++ api/src/models/document.model.js
@@ -0,0 +1,41 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/database");
+
+class Document extends Model {}
+
+Document.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    docType: {
+      type: DataTypes.ENUM(
+        "photo_identite",
+        "titre_sejour",
+        "certificat_scolarite",
+        "diplome",
+        "rib",
+        "justificatif_domicile",
+        "charte_engagement",
+        "autre"
+      ),
+      allowNull: false,
+    },
+    fileUrl: { type: DataTypes.STRING },
+    kycStatus: {
+      type: DataTypes.ENUM("CREATED", "VALIDATION_ASKED", "VALIDATED", "REFUSED"),
+      defaultValue: "CREATED",
+    },
+    statusComment: { type: DataTypes.TEXT },
+    isGeneral: { type: DataTypes.BOOLEAN, defaultValue: true },
+    missionId: { type: DataTypes.INTEGER, allowNull: true },
+    sentAt: { type: DataTypes.DATE },
+  },
+  {
+    sequelize,
+    modelName: "Document",
+    tableName: "documents",
+    timestamps: true,
+  }
+);
+
+module.exports = Document;
