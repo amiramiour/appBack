8026987a02b414f43632d1babde7acc02316d508
amiramiour <amiouramirtahar@gmail.com>
2025-12-28 13:56:23 +0100

    edit list controller

--- api/src/controllers/mission.controller.js
+++ api/src/controllers/mission.controller.js
@@ -22,19 +22,24 @@ exports.create = async (req, res) => {
 
 exports.list = async (req, res) => {
   try {
-    // 🔹 Vérifie le cache
+    //  Cache
     const cached = missionCache.get("missions");
     if (cached) {
       return res.json({ source: "cache", data: cached });
     }
 
-    // 🔹 Si pas dans le cache → récupère depuis la DB
+    //  DB
     const missions = await missionService.getAllMissions();
 
-    // 🔹 Stocke dans le cache pour 30s
-    missionCache.set("missions", missions);
+    //  CONVERSION CRITIQUE (OBLIGATOIRE)
+    const plainMissions = missions.map(m =>
+      m.get({ plain: true })
+    );
 
-    res.json({ source: "db", data: missions });
+    //  Cache sécurisé
+    missionCache.set("missions", plainMissions);
+
+    res.json({ source: "db", data: plainMissions });
   } catch (err) {
     console.error("Erreur dans list missions:", err);
     res.status(500).json({ error: "Erreur interne du serveur" });
